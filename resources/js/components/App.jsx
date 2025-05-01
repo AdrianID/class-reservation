080af9a69118d3fcf78d6b22fe8de91178611b4b
AdrianID <adrianfrizna14@gmail.com>
2025-05-01 20:19:04 +0700

    Initial Commit

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold text-center">
+                Welcome to Laravel React
+            </h1>
+            <p className="text-center mt-4">
+                This is a React component in Laravel
+            </p>
+        </div>
+    );
+}
+
+export default App; 
